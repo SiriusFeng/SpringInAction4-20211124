7dbbe183f6edf72208ca7647709e4f45f27a7634
john <442088637@qq.com>
2021-11-25 21:57:11 +0800

    chapter02:装配bean

---
+++ chapter02/src/main/java/com/study/soundsystem/CompactDisc.java
@@ -0,0 +1,5 @@
+package com.study.soundsystem;
+
+public interface CompactDisc {
+    void play();
+}
---
+++ chapter02/src/main/java/com/study/soundsystem/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.study.soundsystem;
+
+public interface MediaPlayer {
+    void play();
+}
